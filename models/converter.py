2e453903085b45e581cb7fbf378a81734da748a6
Timo <timo.schaffner@stud.hslu.ch>
2025-12-17 15:20:00 +0100

    added new modelv5, best until now, also converted to ncnn

--- models/converter.py
+++ models/converter.py
@@ -1,11 +1,12 @@
 from ultralytics import YOLO
 
-"""# Load a YOLO11n PyTorch model
-model = YOLO("modelv2.pt")
+# Load a YOLO11n PyTorch model
+model = YOLO("modelv5.pt")
 
 # Export the model to NCNN format
-model.export(format="ncnn")  # creates 'modelv2_ncnn_model'
-"""
+model.export(format="ncnn")  # creates 'modelvX_ncnn_model'
+
+
 # Load the exported NCNN model (use correct name!)
 ncnn_model = YOLO("modelv2_ncnn_model")
 
